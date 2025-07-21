55bf3d020d48ddaa715528e8afb4319433393b64
Afra Anjum <lilithkard77@gmail.com>
2025-07-21 23:47:27 +0600

    secured URI

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import {Server} from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 
+
 let server: Server
+const MONGO_URI = process.env.MONGO_URI as string
 
 
 const startServer = async () => {
     try {
-        await mongoose.connect("mongodb+srv://WorkHive:m3QfzhcDDxtdHgmW@cluster0.oo5u4.mongodb.net/tour-management-backend?retryWrites=true&w=majority&appName=Cluster0")
-    
+        await mongoose.connect(MONGO_URI)
+
         console.log("connected to db UwU")
         server = app.listen(5000, ()=>{
             console.log(`server is listening to port http://localhost:5000`)
@@ -21,3 +23,51 @@ const startServer = async () => {
 
 startServer()
 
+process.on("unhandledRejection", (err)=>{
+    console.log("unhandled Rejection detected.... Server shutting down", err);
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        });
+    }
+
+    process.exit(1);
+    
+})
+
+process.on("uncaughtException", (err)=>{
+    console.log("Uncaught Exception detected.... Server shutting down", err)
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        });
+    }
+
+    process.exit(1);
+})
+
+process.on("SIGTERM", ()=>{
+    console.log("SIGTERM SIGNAL RECEIVED.... Server shutting down");
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        });
+    }
+
+    process.exit(1);
+})
+
+process.on("SIGINT", ()=>{
+    console.log("SIGINT SIGNAL RECEIVED.... Server shutting down");
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        });
+    }
+
+    process.exit(1);
+})
